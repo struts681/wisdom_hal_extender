ac9853a0a2daedf5c3949d37b685a252e8e33011
amelia v <amelia@ag2v.com>
2025-06-12 13:37:38 -0400

    update cmakelists, properly add rtc libs

--- src/modules_weak/rtc_weak.c
+++ src/modules_weak/rtc_weak.c
@@ -8,12 +8,10 @@
 
 
 _WEAK_ int w_rtc_init(void) { return UNIMPLEMENTED; } //init i2c, default settings (cap, ampm, bat switchover, etc)
-_WEAK_ int w_rtc_hw_rst(void) { return UNIMPLEMENTED; } //hardware FULL reset - see datasheet for initial condx
+//_WEAK_ int w_rtc_hw_rst(void) { return UNIMPLEMENTED; } //hardware FULL reset - see datasheet for initial condx
 
-_WEAK_ int w_rtc_hour_format_set(bool timeformat) { return UNIMPLEMENTED; } //24hr or 12hr am/pm (ew)
-_WEAK_ int w_rtc_hour_format_get(bool *timeformat) { return UNIMPLEMENTED; }
+//_WEAK_ int w_rtc_hour_format_set(bool timeformat) { return UNIMPLEMENTED; } //24hr or 12hr am/pm (ew)
+//_WEAK_ int w_rtc_hour_format_get(bool *timeformat) { return UNIMPLEMENTED; }
 
-_WEAK_ int w_rtc_datetime_set(w_rtc_datetime_t *datetime) { return UNIMPLEMENTED; } //just all at once
-_WEAK_ int w_rtc_datetime_get(w_rtc_datetime_t *datetime) { return UNIMPLEMENTED; }
 
 
