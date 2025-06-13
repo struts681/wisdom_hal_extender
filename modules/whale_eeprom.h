962caf444237e2d6b53304a0fb4d40961e3406f9
amelia v <amelia@ag2v.com>
2025-06-13 15:22:14 -0400

    create header file & weak c file

---
+++ modules/whale_eeprom.h
@@ -0,0 +1,8 @@
+#ifndef WHALE_EEPROM_H
+#define WHALE_EEPROM_H
+
+
+//will add function prototypes here
+
+
+#endif
---
+++ src/modules_weak/eeprom_weak.c
@@ -0,0 +1,6 @@
+
+#include "whale_rtc.h"
+
+#define _WEAK_ __attribute__((weak))
+
+#define UNIMPLEMENTED (-1)
